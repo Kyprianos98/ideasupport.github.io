c81cf51bb64ab8312d9955588414dcbafcb4dbb1
Kyprianos98 <54856318+Kyprianos98@users.noreply.github.com>
2023-12-20 16:12:17 +0200

    Update e-shop.js

--- e-shop.js
+++ e-shop.js
@@ -23,57 +23,41 @@ function calculateTotal() {
 	return total;
 }
 
-// Code for the product page
-let products = [
-	{
-		name: "Sewing Machine",
-		price: 299.99,
-		description: "A high-quality sewing machine for all your sewing needs."
-	},
-	{
-		name: "Thread Set",
-		price: 19.99,
-		description: "A set of 50 spools of high-quality thread in a variety of colors."
-	},
-	{
-		name: "Scissors",
-		price: 9.99,
-		description: "A pair of sharp, durable scissors for all your cutting needs."
-	}
-];
-
-// Display products on the page
-function displayProducts() {
-	let productContainer = document.getElementById("product-container");
-	for (let i = 0; i < products.length; i++) {
-		let product = products[i];
-		let productElement = document.createElement("div");
-		productElement.classList.add("product");
-		productElement.innerHTML = `
-			<h2>${product.name}</h2>
-			<p>${product.description}</p>
-			<p>$${product.price.toFixed(2)}</p>
-			<button onclick="addItem(${JSON.stringify(product)})">Add to Cart</button>
-		`;
-		productContainer.appendChild(productElement);
-	}
+// Show cart
+function showCart() {
+	let cartContainer = document.createElement("div");
+	cartContainer.id = "cart-container";
+	cartContainer.innerHTML = `
+		<h2>Shopping Cart</h2>
+		<button onclick="hideCart()">Close</button>
+		<div id="cart-items"></div>
+		<p>Total: $<span id="total"></span></p>
+	`;
+	document.body.appendChild(cartContainer);
+	displayCart();
 }
 
-// Code for the shopping cart page
-function displayCart() {
+// Hide cart
+function hideCart() {
 	let cartContainer = document.getElementById("cart-container");
-	cartContainer.innerHTML = "";
+	document.body.removeChild(cartContainer);
+}
+
+// Display cart items
+function displayCart() {
+	let cartItems = document.getElementById("cart-items");
+	cartItems.innerHTML = "";
 	for (let i = 0; i < cart.length; i++) {
 		let item = cart[i];
 		let itemElement = document.createElement("div");
 		itemElement.classList.add("cart-item");
 		itemElement.innerHTML = `
-			<h2>${item.name}</h2>
+			<h3>${item.name}</h3>
 			<p>$${item.price.toFixed(2)}</p>
 			<button onclick="removeItem(${JSON.stringify(item)})">Remove</button>
 		`;
-		cartContainer.appendChild(itemElement);
+		cartItems.appendChild(itemElement);
 	}
 	let totalElement = document.getElementById("total");
-	totalElement.innerHTML = `$${calculateTotal()}`;
+	totalElement.innerHTML = calculateTotal().toFixed(2);
 }
